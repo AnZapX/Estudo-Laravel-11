5197e09fe7e9a97a64fa050e215eb783654b6f51
AnZapX <andreaszapala98@gmail.com>
2024-06-26 20:54:13 -0300

    Primeiro Código!

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/contato', [SiteController::class, 'contact']);
 
 Route::get('/', function () {
     return view('welcome');
